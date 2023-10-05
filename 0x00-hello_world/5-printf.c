9848db8f21aeb6f528a97c095796dff30d48c161
ntsakozinhle <ntsakosambo1@gmail.com>
2023-10-05 06:09:36 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,9 +5,10 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main()
 {
-	printf("with proper grammar, but the outcome is a piece of art,")
+	printf("with proper grammar, but the outcome is a piece of art,");
 
-	return (0)
+	return (0);
 }
