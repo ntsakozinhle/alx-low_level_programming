43642e9549f51eb23dcb27f5d646ee9045c0eb0d
ntsakozinhle <ntsakosambo1@gmail.com>
2023-10-05 05:38:19 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-int main(void)
 
+/**
+ * int - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	char* str1 = "\"Programming is like building a multilingual puzzle";
-	
+	char *str1 = "\"Programming is like building a multilingual puzzle";
 	puts(str1);
 
-	return 0;
-
+	return (0);
 }
