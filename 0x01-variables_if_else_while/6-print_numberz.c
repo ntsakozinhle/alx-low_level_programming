afe4b5b3b2d0db5782aad619f8f1d62dade32ae2
ntsakozinhle <ntsakosambo1@gmail.com>
2023-10-06 12:30:16 -0700

    Numeb3rs

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -a program that prints all single
+ * digit numbers of base 10 starting from 0,
+ * followed by a new line
+ *
+ * cannot use char variable, cannot use printf or
+ * puts
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0' ; num <= '9' ; num++)
+		putchar(num + '0');
+
+	putchar('\n');
+
+	return (0);
+}
