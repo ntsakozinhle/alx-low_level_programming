fc866ea2354fc8f7c21bd2e35ec1ddd0d4aa642e
ntsakozinhle <ntsakosambo1@gmail.com>
2023-10-06 11:14:46 -0700

    lowercase alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - this program prints the alphabet in
+ * lowercase
+ *
+ * cannot use printf
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alpha;
+
+	for(alpha = 'a' ; alpha <= 'z' ; alpha++)
+	
+	{
+		putchar(alpha);
+	}
+	
+	return (0);
+}
