6915fad50c4b14cea8699ed6534552151eba3e0a
ntsakozinhle <ntsakosambo1@gmail.com>
2023-10-06 12:07:15 -0700

    Numeb3rs

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - a program that prints all single digit
+ * numbers of base 10 starting from 0
+ * folled by a new line
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char num;
+
+	for (num = '0' ; num <= '9' ; num++)
+		putchar(num);
+
+	return (0);
+}
