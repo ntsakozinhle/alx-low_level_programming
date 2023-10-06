9bd167a0da2e77426a1679e2279a44fba3e51b7f
ntsakozinhle <ntsakosambo1@gmail.com>
2023-10-06 12:40:28 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - a program that writes the lowercase
+ * alphabet in reverse, followed by a new line
+ *
+ * cannot use printf or puts
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char ahpla;
+
+	for (ahpla = 'a' ; ahpla <= 'z' ; ahpla--)
+		putchar(ahpla);
+
+	putchar('\n');
+
+	return (0);
+}
