ca83bc1b2ec692c9013cb6dfb3353f1de08e8585
ntsakozinhle <ntsakosambo1@gmail.com>
2023-10-08 06:37:35 -0700

    Do what you do better

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - a program that prints all possible different
+ * combinations of three digits
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = '0' ; a < '9' ; a++)
+
+	{
+		for (b = a + 1 ; b <= '9' ; b++)
+
+		{
+			for (c = b + 1 ; c <= '9'; c++)
+
+			{
+				if (c != b && b != a)
+
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+
+				if (a == '7' && b == '8' && c == '9')
+
+				continue;
+
+				putchar(',');
+				putchar(' ');
+
+				}
+
+			}
+
+		}
+
+		putchar('\n');
+		return (0);
+	}
+}
