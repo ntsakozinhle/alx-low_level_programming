b59867273b8eccc1d5951845ab7565dd1224997a
ntsakozinhle <ntsakosambo1@gmail.com>
2023-10-11 12:47:06 -0700

    nature made natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - a program that computes and prints the sum of all multiples
+ * of 3 or 5, followed by a new line
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int sum = 0;
+	int i;
+
+	for (i = 1; i < 1024 ; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+			sum = sum + i;
+	}
+	printf("%d\n", sum);
+}
