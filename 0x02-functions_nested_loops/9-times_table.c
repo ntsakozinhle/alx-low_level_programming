02cfff98ed1b293daf18f31625aad3d1a9066f28
ntsakozinhle <ntsakosambo1@gmail.com>
2023-10-11 09:33:48 -0700

    Times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table -prints the 9 times table,
+ * starting with 0
+ *
+ * Return: Always 0
+ */
+
+void times_table(void)
+{
+	int a;
+	int b;
+	int result;
+
+	for (a = 0; a < 10 ; a++)
+	{
+		for (b = 0; b < 10 ; b++)
+		{
+			result = a * b;
+			printf("%d", result);
+		}
+	}
+	printf("\n");
+}
