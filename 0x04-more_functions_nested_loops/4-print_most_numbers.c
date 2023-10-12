67f5a4fe26bf059ab97dd3e20a5dd28e66e9ec1d
ntsakozinhle <ntsakosambo1@gmail.com>
2023-10-12 04:11:08 -0700

    belive in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,22 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_most_numbers - write a function that prints numbers,
- * followed by a new line
+ * followed by a new line, exclude 2 and 4
  *
- * exclude 2 and 4
- *
- * Return: always 0
+ * Return: void
  */
 
 void print_most_numbers(void)
 {
 	int p;
 
-	for (p = '0' ; p <= 9 ; p++)
+	for (p = 0 ; p <= 9 ; p++)
 	{
 		if (p != 2 && p != 4)
-			_putchar(p);
+			_putchar(p + '0');
 	}
 	_putchar('\n');
 }
