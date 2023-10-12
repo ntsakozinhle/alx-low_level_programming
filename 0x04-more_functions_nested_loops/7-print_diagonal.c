8d3d486cfa79646fdd7a746430609c8179f3089c
ntsakozinhle <ntsakosambo1@gmail.com>
2023-10-12 06:06:22 -0700

    diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -21,9 +21,9 @@ void print_diagonal(int n)
 		int p;
 		int q;
 
-		for (p = 0; p < n ; p++)
+		for (p = 1; p < n ; p++)
 		{
-			for (q = 0 ; q < p ; q++)
+			for (q = 0 ; q <= p ; q++)
 			{	
 				_putchar(' ');
 			}
