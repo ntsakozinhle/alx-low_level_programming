c82dfae195ea8fec13f33934a248c3c0000cccc9
ntsakozinhle <ntsakosambo1@gmail.com>
2023-10-12 18:27:16 -0700

    numbers have life

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_numbers - a function that prints an integar
+ *
+ * @n: parameter to be checked
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	unsigned int i = n;
+	
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+}
