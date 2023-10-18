8e82eed8164050bb0a2a18b8e14dcaf830a83ef8
ntsakozinhle <ntsakosambo1@gmail.com>
2023-10-18 12:53:38 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,16 +14,15 @@
 int _strcmp(char *s1, char *s2)
 
 {
-	int a = 0;
+	int a;
 
-	while (s1[a] != '\0' || s2[a] != '\0')
+	for (a = 0 ; s1[a] != '\0' || s2[a] != '\0' ; a++)
 	{
 		if (s1[a] > s2[a])
 			return (1);
 
 		else if (s1[a] < s2[a])
 			return (-1);
-		a++;
 	}
 	return (0);
 }
