e42fb61ac958b2286caa63a24326157720c618f6
ntsakozinhle <ntsakosambo1@gmail.com>
2023-10-23 05:42:51 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -15,19 +15,11 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int a;
-	unsigned int dest_size = sizeof(dest);
 
-	if (dest_size == 0)
-	{
-		return (dest);
-	}
-
-	for (a = 0 ; a < n && src[a] != '\0' && a < dest_size - 1 ; a++)
+	for (a = 0 ; a < n ; a++)
 	{
 		dest[a] = src[a];
 	}
-
-		dest[a] = '\0';
 
 	return (dest);
 }
