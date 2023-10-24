a92e78707c90f4f34eee8cc91076e16ce09f40ae
ntsakozinhle <ntsakosambo1@gmail.com>
2023-10-23 18:53:21 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strstr - write a function thatloactes a substring
+ *
+ * @needle: parameter to be checked
+ * @haystack: parameter to be checked
+ *
+ * Return: haystack
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *find = needle;
+	char *in_a = haystack;
+
+	if (haystack == NULL || needle == NULL)
+	{
+		return (NULL);
+	}
+
+	while (*haystack != '\0')
+	{
+		while (*find != '\0' && *in_a == *find)
+		{
+			in_a++;
+			find++;
+		}
+
+		if (*find == '\0')
+		{
+			return (haystack);
+		}
+			haystack++;
+
+	}
+	return (NULL);
+}
