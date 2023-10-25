a92ad3e9a254c121a9a42242293d14f4f2cc5d29
ntsakozinhle <ntsakosambo1@gmail.com>
2023-10-25 10:14:46 -0700

    impossible to eradicate

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -20,11 +20,11 @@ int _pow_recursion(int x, int y)
 
 	else if (y > 0)
 	{
-		return (1 + _pow_recursion(x, y));
+		return (x * _pow_recursion(x, y - 1));
 	}
 
 	else
 	{
-		return (0);
+		return (1);
 	}
 }
