1eca4b93dbad38e6148f259db39d30fac2a4140c
ntsakozinhle <ntsakosambo1@gmail.com>
2023-10-25 07:17:26 -0700

    in my dreams we are together

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,22 +11,10 @@
 
 void _print_rev_recursion(char *s)
 {
-	int count = 0;
-
 	if (*s != '\0')
 	{
-		count++;
-
-		if (*s < count / 2)
-		{
-			int d;
-
-			d = *s;
-			*s = s[count - 1 - d];
-			s[count - 1 - d] = d;
-		}
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-		_print_rev_recursion(s - 1);
 	}
 
 	else
