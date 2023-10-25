58f8c2e784c319a7ca651de2faa3f51822ae0fcb
ntsakozinhle <ntsakosambo1@gmail.com>
2023-10-25 10:26:06 -0700

    the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * _sqrt_recursion - a function that returns the natural
+ * square root of a number
+ *
+ * @n: parameter for integer
+ *
+ * Return: 0
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n / n < 0)
+	{
+		return (-1);
+	}
+
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n / _sqrt_recursion(n - 1));
+	}
+}
