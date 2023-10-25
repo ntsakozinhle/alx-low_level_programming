1344fcf558816fdc189b78eb14bd6f9b232a2071
ntsakozinhle <ntsakosambo1@gmail.com>
2023-10-25 07:40:01 -0700

    dreams feel real while we're in them

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen_recursion - a function that writes the length
+ * of a string
+ *
+ * @s: paramter for string in pointer
+ *
+ * Return: s
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		putchar(*s);
+		_strlen_recursion(s + 1);
+	}
+	return (*s);
+}
