25f8e1a7edef860a8bb89636a9b0ad26d99c9706
ntsakozinhle <ntsakosambo1@gmail.com>
2023-10-25 07:08:12 -0700

    in my dreams we are together

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - a function that prints a string in reverse
+ *
+ * @s: parameter for string
+ *
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int count = 0;
+
+	if (*s != '\0')
+	{
+		if (*s < count / 2)
+		{
+			int d;
+
+			d = *s;
+			*s = s[count - 1 - d];
+			s[count - 1 - d] = d;
+		}
+		_putchar(*s);
+		_print_rev_recursion(s + 1);
+		count++;
+	}
+
+	else
+	{
+		_putchar('\n');
+	}
+}
