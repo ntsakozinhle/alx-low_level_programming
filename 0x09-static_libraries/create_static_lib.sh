f1d2ba7034034fb28e495f07c497cf5e1d337ec6
ntsakozinhle <ntsakosambo1@gmail.com>
2023-10-27 07:48:40 -0700

    without libraries we have no past and no future

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc -c -std=gnu89 *.c | ar rc liball.a *o | gcc -std=gnu89 main.c -o liball.a -L. -lall
+gcc -c -std=gnu89 *.c | ar rc liball.a *o | gcc -std=gnu89 main.c -o liball.a -L. -lall | rm *.o
