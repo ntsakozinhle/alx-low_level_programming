871c91d62c952d3af295a853e7e407e63aaf2261
ntsakozinhle <ntsakosambo1@gmail.com>
2023-10-27 08:44:29 -0700

    what they call you

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - a program that prints its name, followed by a new line
+ *
+ * Return: void
+ */
+
+int main(int argc, char *argv[])
+{
+	return (0);
+}
