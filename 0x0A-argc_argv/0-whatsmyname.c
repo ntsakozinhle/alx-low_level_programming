16f08625b9bd59ac188c9938fd7bebfb4acb3f6b
ntsakozinhle <ntsakosambo1@gmail.com>
2023-10-27 09:00:50 -0700

    what they call you

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,6 +4,9 @@
 /**
  * main - a program that prints its name, followed by a new line
  *
+ * @argc: parameter for number of strings
+ * @argv: parameter for array of strings
+ *
  * Return: void
  */
 
@@ -11,7 +14,13 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	printf("%s\n", argv[i]);
+	if (argc > 1)
+	{
+		for (i = 0 ; i < argc ; i++)
+		{
+			printf("%s\n", argv[i]);
+		}
+	}
 
 	return (0);
 }
