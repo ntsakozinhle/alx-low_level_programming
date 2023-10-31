36ea9c22a424dc57bc79a2886f47f72b62519cbe
ntsakozinhle <ntsakosambo1@gmail.com>
2023-10-31 06:21:17 -0700

    he who is not courageous in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,9 +20,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 		s1 = "";
+	return (NULL);
 
 	if (s2 == NULL)
 		s2 = "";
+	return (NULL);
 
 	for (a = 0; s1[a] != '\0'; a++)
 		s1len++;
