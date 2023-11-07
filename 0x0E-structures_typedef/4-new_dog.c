c60712f0570d8e496958947af8691c663f032803
ntsakozinhle <ntsakosambo1@gmail.com>
2023-11-07 03:51:40 -0800

    wrong side of the door

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog - new variable for strcut dog
@@ -16,9 +17,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (new_dog != NULL)
 	{
-		new_dog->name = name;
+		new_dog->name = strdup(name);
 		new_dog->age = age;
-		new_dog->owner = owner;
+		new_dog->owner = strdup(owner);
 	}
 	return (new_dog);
 }
