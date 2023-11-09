a1ff97f260568a8ffaffcecc194dab7ed6379aaf
ntsakozinhle <ntsakosambo1@gmail.com>
2023-11-09 05:33:28 -0800

    Beauty is a veriable

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,13 +13,15 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
-	int sum;
+	int sum = 0;
 	va_list args;
 
 	if (n == 0)
 	{
 		return (0);
 	}
+
+	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
