c0f1cf640580012f995d656d1456b63775254146
ntsakozinhle <ntsakosambo1@gmail.com>
2023-11-09 11:30:30 -0800

    value of a variable

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,42 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - a function that prints numbers
+ * @separator: parameter for string
+ * @n: number of integers being passed
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int a;
+	int str;
+	va_list args;
+
+	if (separator == NULL)
+	{
+		return;
+	}
+
+	va_start(args, n);
+
+	for (a = 0; a < n; a++)
+	{
+		str = va_arg(args, int);
+
+		if (a == n - 1)
+		{
+			printf("%d", str);
+		}
+
+		else
+		{
+			printf("%d%s", str, separator);
+		}
+	}
+	va_end(args);
+
+	printf("\n");
+}
