460ca09047a22113ba45f5b2b213499e42b1fb28
ntsakozinhle <ntsakosambo1@gmail.com>
2023-11-09 16:42:57 -0800

    Value of a variable

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,54 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * print_all - a function that prints everything
+ * @format: lists all arguments passed to the function
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	int b, check_stat;
+	char *str;
+	va_list print;
+
+	va_start(print, format);
+	b = 0;
+	while (format != NULL && format[b] != '\0')
+	{
+		switch (format[b])
+		{
+			case 'i':
+				printf("%i", va_arg(print, int));
+				check_stat = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(print, double));
+				check_stat = 0;
+				break;
+			case 'c':
+				printf("%c", va_arg(print, int));
+				check_stat = 0;
+				break;
+			case 's':
+				str = va_arg(print, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				break;
+			default:
+				check_stat = 1;
+				break;
+		}
+		if (format[b + 1] != '\0' && check_stat == 0)
+		{
+			printf(", ");
+		}
+		b++;
+	}
+	printf("\n");
+	va_end(print);
+}
