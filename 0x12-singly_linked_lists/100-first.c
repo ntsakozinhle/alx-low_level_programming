e1d05f7ca9d4733b04b9988bdd9d17cbb8b6bdb2
ntsakozinhle <ntsakosambo1@gmail.com>
2023-11-17 06:10:13 -0800

    The hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+void print_first(void) __attribute__((constructor));
+
+/**
+ * print_first - a function that runs before the main function
+ */
+
+void print_first(void)
+{
+	printf("You're beat! and yet you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
