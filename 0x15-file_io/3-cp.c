3fc76fba57b2d220d31f3cd8d5ade0ae5fc7bc89
ntsakozinhle <ntsakosambo1@gmail.com>
2023-11-28 18:43:51 -0800

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 664);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
 
 	characters = 1024;
@@ -49,7 +49,7 @@ int main(int argc, char *argv[])
 		characters = read(file_from, buf, 1024);
 		if (characters == -1)
 			error_file(-1, 0, argv);
-		written = write(file_to, buf, 1024);
+		written = write(file_to, buf, characters);
 		if (written == -1)
 			error_file(0, -1, argv);
 	}
@@ -60,6 +60,7 @@ int main(int argc, char *argv[])
 		exit(100);
 	}
 	err_close = close(file_to);
+	if (err_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 		exit(100);
