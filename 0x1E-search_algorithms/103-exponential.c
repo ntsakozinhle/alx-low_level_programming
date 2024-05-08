d0b1a17c6eef0a6869671313fc70224f6b95c06b
ntsakozinhle <ntsakosambo1@gmail.com>
2024-05-07 20:42:07 -0700

    Exponential search

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,5 +1,5 @@
 #include "search_algos.h"
-  
+
 /**
  * binary_searching - searches for a value in a sorted array using binary
  * @array: pointer to the first element of the array
@@ -11,30 +11,30 @@
  */
 int binary_searching(int *array, size_t low, size_t high, int value)
 {
-        size_t mid, i;
+	size_t mid, i;
 
-        while (low <= high)
-        {
-                mid = (low + high) / 2;
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
 
-                printf("Searching in array: ");
-                for (i = low; i <= high; i++)
-                {
-                        printf("%d", array[i]);
-                        if (i < high)
-                                printf(", ");
-                }
-                printf("\n");
+		printf("Searching in array: ");
+		for (i = low; i <= high; i++)
+		{
+			printf("%d", array[i]);
+			if (i < high)
+				printf(", ");
+		}
+		printf("\n");
 
-                if (array[mid] == value)
-                        return (mid);
-                else if (array[mid] < value)
-                        low = mid + 1;
-                else
-                        high = mid - 1;
-        }
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
 
-        return (-1);
+	return (-1);
 }
 
 /**
@@ -63,5 +63,5 @@ int exponential_search(int *array, size_t size, int value)
 	high = (bound < size - 1) ? bound : size - 1;
 
 	printf("Value found between indexes [%lu] and [%lu]\n", low, high);
-	return binary_searching(array, low, high, value);
+	return (binary_searching(array, low, high, value));
 }
